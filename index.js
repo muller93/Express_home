cf66eb590434cb7a9b0a8084d93c6381a3189f77
Norbert Muller <mullernorbert93@gmail.com>
2018-04-02 22:06:47 +0200

    practice

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 let express = require('express');
 let app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello world');
+app.get('/:id', (req, res) => {
+  res.send('The id you specified is ' + req.params.id);
+});
+
+app.post('/hello', (req, res) => {
+  res.send('You just called the post method at');
 });
 
 app.listen(3000);
